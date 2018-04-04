c35786c55f924d0cef963e900da836bef0f23bbe
jnware7 <jnware7@gmail.com>
2018-04-04 16:03:18 -0700

    added state to the nav popular component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,9 +1,30 @@
 var React = require('react')
 
 class Popular extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      selectedLanguage:'All'
+    };
+    this.updateLan = this.updateLan.bind(this)
+  }
+
+  updateLan(lang){
+    this.setState(()=>{ return {selectedLanguage:lang} })
+  }
   render(){
+
+    var languages = ['All','Javascript','Ruby', 'Java','CSS','Python']
+    console.log('whats the context',this)
     return(
-      <div> Popular!</div>
+      <ul className='languages'>
+      {
+        languages.map((lang)=> <li
+        style = { lang === this.state.selectedLanguage ? { color: 'blue' } : null }
+        onClick = {this.updateLan.bind(null, lang)}
+        key={lang}> {lang} </li> )
+      }
+      </ul>
     )
   }
 }
