7280d8deb18a637690fe98d5178eb85909ace5a6
jnware7 <jnware7@gmail.com>
2018-04-19 10:35:23 -0700

    Rendered ui component for ajax requested data

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -16,6 +16,35 @@ const SelectLanguage = (props) => {
     </ul>
     )
 }
+const RepoGrid = (props) => {
+  return (
+    <ul className='popular-list'>
+      {props.repos.map((repo,index)=>{
+        return(
+          <li key={repo.name} className='popular-item'>
+            <div className='popular-rank'>#{index + 1}</div>
+            <ul className='space-list-items'>
+              <li>
+                <img
+                  className='avatar'
+                  src={repo.owner.avatar_url}
+                  alt={'Avatar for' + repo.owner.login}
+                />
+              </li>
+              <li><a href={repo.html_url}>{repo.name}</a></li>
+              <li>@{repo.owner.login}</li>
+              <li>{repo.stargazers_count} stars</li>
+            </ul>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+RepoGrid.propTypes = {
+  repos: PropTypes.array.isRequired
+}
 
 SelectLanguage.propTypes = {
   selectedLanguage: PropTypes.string.isRequired,
@@ -27,14 +56,14 @@ class Popular extends React.Component {
     super(props);
     this.state = {
       selectedLanguage:'All',
-      repos: null
+      repos:null
     };
     this.updateLan = this.updateLan.bind(this)
   }
 
   componentDidMount () {
     //AJAX
-
+    this.updateLan(this.state.selectedLanguage)
   }
 
   updateLan(lang){
@@ -60,10 +89,16 @@ class Popular extends React.Component {
            selectedLanguage={this.state.selectedLanguage}
            onSelect={this.updateLan}
            />
-           {JSON.stringify(this.state.repos)}
+
+          {! this.state.repos
+            ? <p> LOADING </p>
+            : <RepoGrid repos={this.state.repos} />
+          }
+
         </div>
     )
   }
 }
 
 module.exports = Popular;
+          //  {JSON.stringify(this.state.repos)}
